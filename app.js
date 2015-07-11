86ece8a57e4fb5b47326111b26946e2f4943e97b
echicken <echicken@bbs.electronicchicken.com>
2015-07-11 02:49:56 -0400

    Trust proxy

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('port', 3000);
 
+app.enable('trust proxy');
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
